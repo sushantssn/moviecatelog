8f39cbd23fe9ef147273f25d9f69d41cab96fa78
sushantssn <sushant.nalawade@yahoo.in>
2022-09-11 11:11:51 +0530

    Initial commit for moviecatelogservice

---
+++ src/main/java/com/movie/catelog/demo/MovieCatelogApplication.java
@@ -0,0 +1,13 @@
+package com.movie.catelog.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatelogApplication.class, args);
+	}
+
+}
