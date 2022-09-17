e74efbaf4fb06fa822c80734ab0c73899b040eaa
Sushant <sushant.nalawade@yahoo.in>
2022-09-18 00:07:52 +0530

    initial commit

--- src/main/java/com/movie/catelog/demo/controller/MovieCatelogController.java
+++ src/main/java/com/movie/catelog/demo/controller/MovieCatelogController.java
@@ -1,8 +1,11 @@
 package com.movie.catelog.demo.controller;
 
 import com.movie.catelog.demo.pojo.CatelogItem;
+import com.movie.catelog.demo.pojo.Rating;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.ClientInfoStatus;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,6 +16,10 @@ public class MovieCatelogController {
 
     @RequestMapping ("/{userId}")
     public List<CatelogItem> getMovies(@PathVariable String userId){
+
+        //get the ratings of the movies
+        List<Rating> ratings = Arrays.asList(new Rating("12234",3),
+                new Rating("12345",5));
         return Collections.singletonList(new CatelogItem("Laagan","Cricket Movie",5));
     }
 
